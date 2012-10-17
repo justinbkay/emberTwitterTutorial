2aa83537742532401c79635b2f1e8b2cda4c122b
Justin B Kay <jkay@nwrecc.org>
2012-10-17 15:53:51 -0600

    Getting the views to work with the new ember code
    Included the improvements made by trek on the controller code

--- js/app.js
+++ js/app.js
@@ -1,22 +1,19 @@
 App = Em.Application.create();
 
 App.SearchTextField = Em.TextField.extend({
-    insertNewline: function() {
-        App.tweetsC.loadTweets();
-    }
+  insertNewline: function() {
+    App.tweetsC.loadTweets();
+  }
 });
 
 App.Tweet = Em.Object.extend({
-    profile_pic: null,
-    user_name: null,
+    profilePic: null,
+    username: null,
     date: null,
     text: null
 });
 
 App.tweetsV = Em.View.extend({
-	loaderBinding: 'App.tweetsC.loader',
-	firstRunBinding: 'App.tweetsC.firstRun',
-	limitBinding: 'App.tweetsC.limit'
 });
 
 App.tweetsC = Em.ArrayController.create({
@@ -25,43 +22,48 @@ App.tweetsC = Em.ArrayController.create({
     loader: false,
     firstRun: true,
     limit: false,
+    
+    loadTweetsCompleted: function(xhr) {
+      if(xhr.status == 400 || xhr.status == 420) {
+        this.set('limit', true);
+      }
+    },
+    
+    loadTweetsSucceeded: function(data) {
+      var tweets = data.results;
+      this.set('content', []);
+      tweets.forEach(function(tweetData) {
+        var d = new Date(tweetData.created_at);
+        var ago = $.timeago(d);
+        var tweet = App.Tweet.create({
+          profilePic: tweetData.profile_image_url,
+          username: tweetData.from_user,
+          date: ago,
+          text: tweetData.text
+        });
+        this.pushObject(tweet);
+      }, this);
+
+      this.set('loader', false);
+    },
+    
     loadTweets: function() {
-      var me = this;
-      var term = me.get('term');
-      var first = me.get('firstRun');
+      var term = this.get('term'),
+          first = this.get('firstRun');
       //show loader
-      me.set('loader', true);
+      this.set('loader', true);
       //no longer the first run.
       if(first == true)
-        me.set('firstRun', false);
+        this.set('firstRun', false);
 
       if ( term ) {
-        var url = 'http://search.twitter.com/search.json?q=%@&rpp=15&include_entities=true&result_type=recent&callback=?'.fmt(encodeURIComponent(me.get('term')));
+        var url = 'http://search.twitter.com/search.json?q=%@&rpp=15&include_entities=true&result_type=recent&callback=?'.fmt(encodeURIComponent(this.get('term')));
         $.ajax({
           url: url,
           dataType: 'JSON',
-          success: function(data) {
-            var tweets = data.results;
-            me.set('content', []);
-            $(tweets).each(function(index,tweet){
-              //Reformat date
-              var d = new Date(tweet.created_at);
-              var ago = $.timeago(d);
-              var t = App.Tweet.create({
-                profile_pic: tweet.profile_image_url,
-                user_name: tweet.from_user,
-                date: ago,
-                text: tweet.text
-              });
-              me.pushObject(t);
-            });
-            me.set('loader', false);
-          },
-          complete: function(xhr) {
-            if(xhr.status == 400 || xhr.status == 420) {
-              me.set('limit', true);
-            }
-          }
+          context: this,
+          success: this.loadTweetsSucceeded,
+          complete: this.loadTweetsCompleted
         });
       }
     }
