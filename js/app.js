2972698e1a1980dd04d4fd931262690c6cf1b382
Justin B Kay <jkay@nwrecc.org>
2012-10-19 14:35:39 -0600

    Breaking up the deeply nested view into smaller views for better readability
    Added a no results state as well

--- js/app.js
+++ js/app.js
@@ -16,22 +16,32 @@ App.Tweet = Em.Object.extend({
 App.tweetsV = Em.View.extend({
 });
 
+App.helperView = Em.View.extend({
+});
+
+App.loadingView = Em.View.extend({
+});
+
+App.limitView = Em.View.extend({});
+
+App.noresultsView = Em.View.extend({});
+
 App.tweetsC = Em.ArrayController.create({
     content: [],
     term: '',
     loader: false,
     firstRun: true,
     limit: false,
-    
+    noresults: false,
+
     loadTweetsCompleted: function(xhr) {
       if(xhr.status == 400 || xhr.status == 420) {
         this.set('limit', true);
       }
     },
-    
+
     loadTweetsSucceeded: function(data) {
       var tweets = data.results;
-      this.set('content', []);
       tweets.forEach(function(tweetData) {
         var d = new Date(tweetData.created_at);
         var ago = $.timeago(d);
@@ -45,19 +55,24 @@ App.tweetsC = Em.ArrayController.create({
       }, this);
 
       this.set('loader', false);
+      if (this.content.length === 0) {
+        this.set('noresults', true);
+      }
+
     },
-    
+
     loadTweets: function() {
       var term = this.get('term'),
           first = this.get('firstRun');
       //show loader
       this.set('loader', true);
       //no longer the first run.
-      if(first == true)
+      if(first === true)
         this.set('firstRun', false);
-
+      this.set('noresults', false);
       if ( term ) {
         var url = 'http://search.twitter.com/search.json?q=%@&rpp=15&include_entities=true&result_type=recent&callback=?'.fmt(encodeURIComponent(this.get('term')));
+        this.set('content', []);
         $.ajax({
           url: url,
           dataType: 'JSON',
